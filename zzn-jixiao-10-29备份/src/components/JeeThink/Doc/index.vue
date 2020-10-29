25fa7bbbbe9c5fdb71234493ee72e9aa0ac1b9f9
e <1403523789@qq.com>
2020-10-29 21:26:10 +0800

    第一次提交

---
+++ zzn-jixiao-10-29备份/src/components/JeeThink/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="link" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JeeThinkDoc',
+  data() {
+    return {
+      url: 'https://item.taobao.com/item.htm?id=629682899366'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
